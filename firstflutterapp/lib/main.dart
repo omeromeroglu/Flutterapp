17cf64072959008966ce4ce9a13a649d52e8a7cd
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-07-10 22:19:19 +0200

    second try

--- firstflutterapp/lib/main.dart
+++ firstflutterapp/lib/main.dart
@@ -9,6 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Material(child: Text("Hello world commit")));
+    return MaterialApp(home: Material(child: Text("Hello world commit 2")));
   }
 }
